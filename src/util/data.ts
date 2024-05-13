908f231295da60a414701b8a9928c3ec47db79f0
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-05-13 20:39:39 +0100

    chore: include project data type definitions :green_heart:

--- src/util/data.ts
+++ src/util/data.ts
@@ -1,4 +1,4 @@
-import { PortfolioItem } from "./definition";
+import { PortfolioItem, ProjectDataType } from "./definition";
 
 export const PortfolioData: PortfolioItem[] = [
   {
@@ -15,5 +15,22 @@ export const PortfolioData: PortfolioItem[] = [
     id: 3,
     name: "LinkedIn",
     uri: "https://www.linkedin.com/in/nnamdi-azubuike/"
+  }
+]
+
+export const ProjectData: ProjectDataType[] = [
+  {
+    id: 1,
+    name: "Triple R Salon",
+    desc: "Triple R is a renowned full- service beauty salon that is synonymous with exceptional quality and unparalleled luxury.",
+    action: "View Project",
+    uri: "https://www.triplerunisexsalon.com/",
+  },
+  {
+    id: 2,
+    name: "Hostelpaddy",
+    desc: "Your No.1 Solution for hostel accommodation An innovating way to relieve you of the hassle of hostel- hunting.",
+    action: "View Project",
+    uri: "https://hostelpaddy.netlify.app/"
   }
 ]
