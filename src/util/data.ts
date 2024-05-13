6f3917692602c7c1b009279b99dc49e3660d737f
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-05-13 14:15:47 +0100

    feat: temp data :tada:

---
+++ src/util/data.ts
@@ -0,0 +1,19 @@
+import { PortfolioItem } from "./definition";
+
+export const PortfolioData: PortfolioItem[] = [
+  {
+    id: 1,
+    name: "Github",
+    uri: "https://github.com/iamclement1",
+  },
+  {
+    id: 2,
+    name: "Twitter",
+    uri: "https://twitter.com/_iamclement_",
+  },
+  {
+    id: 3,
+    name: "LinkedIn",
+    uri: "https://www.linkedin.com/in/nnamdi-azubuike/"
+  }
+]
