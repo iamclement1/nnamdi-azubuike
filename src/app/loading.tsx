ec0934cfbc2ee5a6d5009371a5a89254f27cb431
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-05-16 11:53:35 +0100

    chore: improve streaming :green_heart:

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,12 @@
 import { Loader } from 'lucide-react';
 
 export default function Loading() {
-  return <div className="mx-auto flex flex-col items-center justify-center bg-black/20 text-gray-400">
-    <Loader className='animate-spin text-gray-400' />
-    <p>Please wait, page is loading...</p>
-  </div>;
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <div className="flex flex-col items-center text-gray-400">
+        <Loader className='animate-spin' />
+        <p className="mt-4">Please wait, page is loading...</p>
+      </div>
+    </div>
+  );
 }
