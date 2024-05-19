88bcb921a3ac387e6f9c8d2078f2a0ec88477ca3
iamclement1 <iamnnamdiclement@gmail.com>
2024-05-19 22:21:27 +0100

    chore: display temp data :green_heart:

--- src/app/(dashboard)/articles/page.tsx
+++ src/app/(dashboard)/articles/page.tsx
@@ -1,3 +1,10 @@
+import React from "react";
+import { HomeArticles } from "@/components/ui/LandingPage/Articles";
+
 export default function Page() {
-  return <p>Articles Page</p>
+  return (
+    <main className="text-white pt-28 md:px-8 px-3">
+      <HomeArticles />
+    </main>
+  );
 }
