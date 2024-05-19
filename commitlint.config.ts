71193ecf71da80ff82cfdc411b3e3d39f930f44a
iamclement1 <iamnnamdiclement@gmail.com>
2024-05-19 17:57:40 +0100

    chore: commit message :green_heart:

---
+++ commitlint.config.ts
@@ -0,0 +1,18 @@
+import conventionalCommit from "./conventionalCommit.json";
+
+const typesEnum = Object.keys(conventionalCommit.types);
+const scopesEnum = Object.keys(conventionalCommit.scopes);
+
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", typesEnum],
+    "scope-case": [2, "always", ["camel-case"]],
+    "scope-enum": [2, "always", scopesEnum],
+    "subject-empty": [2, "never"],
+    "subject-case": [2, "always", ["lower-case"]],
+    "header-max-length": [2, "always", 72],
+  },
+};
+
+export default config;
